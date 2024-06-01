c9915a72f440f345f6af726aa056648f95e59869
jonah-ramponi <jonahramponi2@gmail.com>
2024-06-01 18:25:54 +0100

    removing test script

--- test.py
+++
@@ -1,8 +0,0 @@
-from uk_police_client import CrimesClient
-
-client = CrimesClient()
-
-location = {"lat": 52.629729, "lng": -1.131592}
-date = "2022-02"
-crimes = client.get_street_level_crimes(location, date)
-print(crimes)
